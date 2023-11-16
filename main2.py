e9ca5fb0cea8de4d4ca514ad4e4d003ef5e1d719
Thomas Wilson <111264542+thomasmod@users.noreply.github.com>
2023-11-16 17:13:43 +0500

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,7 +1,8 @@
 import telebot
-from datetime import datetime, timedelta
-from pytz import timezone  # pytz kutubxonasini qo'shing
-import time
+from datetime import datetime
+from pytz import timezone
+import requests
+from bs4 import BeautifulSoup
 
 # Botni tokenini quyidagi o'rniga joylashtiring
 TOKEN = "6751568339:AAFBsO1Jex2szUO7uQ9eGRaagj7y0r5KZT8"
@@ -31,7 +32,6 @@ def handle_time_left(message):
     time_left_str = f"Yangi yilga {days_left} kun, {hours_left} soat, {minutes_left} daqiqa va {seconds_left} soniya qoldi!"
     bot.send_message(message.chat.id, time_left_str)
 
-# Обработчик команды /ping
 @bot.message_handler(commands=['ping'])
 def ping(message):
     start_time = time.time()
@@ -39,6 +39,20 @@ def ping(message):
     end_time = time.time()
     ping_time = end_time - start_time
     bot.send_message(message.chat.id, f"Ping time: {ping_time} seconds")
+
+@bot.message_handler(commands=['get_tashkent_time'])
+def get_tashkent_time(message):
+    # Toshkent vaqti uchun saytga so'rov jo'natish
+    response = requests.get("https://allcalc.ru/node/1868")
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tashkent_time_str = soup.find("span", {"class": "date-time"}).text.strip()
+
+    # Vaqtni formatlash
+    tashkent_time = datetime.strptime(tashkent_time_str, "%d.%m.%Y %H:%M:%S")
+    tashkent_time = tashkent_time.replace(tzinfo=uz_tz)
+
+    # Vaqtni foydalanuvchiga yuborish
+    bot.send_message(message.chat.id, f"Joriy Toshkent vaqti: {tashkent_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
